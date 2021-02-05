49515093a86d5517a918b597ce21938f4ffff563
ArGJolan <argjolan@gmail.com>
2021-02-05 17:10:46 +0100

    Working with timeout

--- 2-ways-serial/arduino/arduino.ino
+++ 2-ways-serial/arduino/arduino.ino
@@ -2,10 +2,6 @@
 #include <SoftwareSerial.h>
 SoftwareSerial fromESP(2, 3);
 
-void debug(String content) {
-  Serial.print("DEBUG_REQUEST_RESULT " + content);
-}
-
 void setup() {
   Serial.begin(9600);
   fromESP.begin(9600);
@@ -14,14 +10,16 @@ void setup() {
 }
 
 String post(String url, String payload) {
-  debug("Post");
+  int count = 0;
 
   Serial.print("POST\n" + url + "\n" + payload);
   String result = "";
 
-  while (result == "") {
+  while (result == "" && count < 500) {
     while (fromESP.available()) {
       result = fromESP.readString();
+      delay(10);
+      count++;
     }
   }
 
@@ -29,14 +27,16 @@ String post(String url, String payload) {
 }
 
 String get(String url) {
-  debug("Get");
+  int count = 0;
 
   Serial.print("GET\n" + url + "\n");
   String result = "";
 
-  while (result == "") {
+  while (result == "" && count < 500) {
     while (fromESP.available()) {
       result = fromESP.readString();
+      delay(10);
+      count++;
     }
   }
 
@@ -44,20 +44,9 @@ String get(String url) {
 }
 
 void loop() {
-  Serial.print("http://192.168.178.36:4365/humidity");
+  long randNumber = random(100);
 
-  String result = "";
+  String result = post("http://192.168.178.36:4365/humidity", String("{ \"value\": ") + randNumber + String(" }"));
 
-  while (result == "") {
-    while (fromESP.available()) {
-      result = fromESP.readString();
-    }
-  }
-  Serial.print("http://192.168.178.36:4365/DONE" + result);
   delay(60000);
-  // long randNumber = random(100);
-
-  // String result = post("http://192.168.178.36:4365/humidity", String("{ \"value\": ") + randNumber + String(" }"));
-  
-  // debug("RESULT: " + result);
 }
--- 2-ways-serial/esp8266/esp8266.ino
+++ 2-ways-serial/esp8266/esp8266.ino
@@ -6,14 +6,19 @@
 SoftwareSerial fromArduino(2, 0);
 
 void debug(String message) {
-  Serial.print("DEBUG_FROM_ESP: " + message + "\n");
+  if (WiFi.status() == WL_CONNECTED) {
+    get("http://192.168.178.36:4365/debug");
+    get("http://192.168.178.36:4365/debug" + message);
+  //} else {
+  //  Serial.print(message);
+  }
 }
 
 void setup() {
   Serial.begin(9600);
   fromArduino.begin(9600);
 
-  debug("STARTING");
+  debug("Starting");
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     debug("CONNECTING TO WIFI");
@@ -25,7 +30,6 @@ void setup() {
 }
 
 String post(char *url, char *payload) {
-  debug("Sending post to" + String(url));
   HTTPClient http;
 
   http.begin(url);
@@ -36,7 +40,15 @@ String post(char *url, char *payload) {
 }
 
 String get(char *url) {
-  debug("Sending get to" + String(url));
+  HTTPClient http;
+
+  http.begin(url);
+  int httpCode = http.GET();
+
+  return httpCode + String(":") + http.getString();
+}
+
+String get(String url) {
   HTTPClient http;
 
   http.begin(url);
@@ -54,15 +66,58 @@ void loop() {
 
   if (request != "") {
     if (WiFi.status() == WL_CONNECTED) {
+      debug("GOT MESSAGE" + request);
+      /*
+      ** Format of requests:
+      **
+      ** "GET
+      **  http://totoland.site"
+      **
+      ** "POST
+      **  http://totoland.site
+      **  {\"toto\": 42}"
+      */
+
+      debug("Received request" + request);
+      String result;
+
       // Cancer string convertion
       const char *constRequest = request.c_str();
       char *properRequest = (char *)malloc((strlen(constRequest) + 1) * sizeof(char));
       properRequest = (char*)memset(properRequest, 0, (strlen(constRequest) + 1) * sizeof(char));
       strcpy(properRequest, constRequest);
 
-      String result = get(properRequest);
+      String debugUrl = String("http://192.168.178.36:4365/debug") + String(properRequest);
+      get("http://192.168.178.36:4365/debug");
+
+      // Fetch method & url from request
+      char *method = strtok(properRequest, "\n");
+      char *url = strtok(NULL, "\n");
+
+      debug("METHOD:" + String(method));
+      debug("URL:" + String(url));
+
+      if (strcmp(method, "post") == 0 || strcmp(method, "POST") == 0) {
+        char *payload = strtok(NULL, "\n");
+        debug("PAYLOAD:" + String(payload));
+        if (method == NULL || url == NULL || payload == NULL) {
+          result = "Need to provide method, url and payload on 3 lines";
+        } else {
+          result = post(url, payload);
+        }
+      } else if (strcmp(method, "get") == 0 || strcmp(method, "GET") == 0) {
+        if (method == NULL || url == NULL) {
+          result = "Need to provide method and url on 2 lines";
+        } else {
+          result = get(url);
+        }
+      } else {
+        result = "Method not handled " + String(method);
+      }
 
       Serial.print(result);
+      delay(100);
+      ESP.deepSleep(30e6);
     } else {
       debug("wifierror");
     }
