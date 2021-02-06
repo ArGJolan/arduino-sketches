5e7238e52653ea1c5296ee68fa337223bc2f00a3
ArGJolan <argjolan@gmail.com>
2021-02-06 12:08:08 +0100

    Add POST & GET methods

--- http-standalone-esp8266/esp8266.ino
+++ http-standalone-esp8266/esp8266.ino
@@ -13,21 +13,40 @@ void setup() {
     Serial.println("Waiting for connection");
   }
 }
- 
+
+String post(char *url, char *payload) {
+  HTTPClient http;
+
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  int httpCode = http.POST(payload);
+
+  return httpCode + String(":") + http.getString();
+}
+
+String get(char *url) {
+  HTTPClient http;
+
+  http.begin(url);
+  int httpCode = http.GET();
+
+  return httpCode + String(":") + http.getString();
+}
+
 void loop() {
   delay(5000);
   if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
     HTTPClient http;    //Declare object of class HTTPClient
- 
+
     http.begin("http://192.168.178.20:4242");      //Specify request destination
     http.addHeader("Content-Type", "text/plain");  //Specify content-type header
- 
+
     int httpCode = http.POST("Message from ESP8266");   //Send the request
     String payload = http.getString();                  //Get the response payload
- 
+
     Serial.println(httpCode);   //Print HTTP return code
     Serial.println(payload);    //Print request response payload
- 
+
     http.end();  //Close connection
   } else {
     Serial.println("Error in WiFi connection");
